c8d96d0647cb09f776d078dabdacb0a3818d5636
Elint Eerie <96844092+elinteerie@users.noreply.github.com>
2022-09-20 12:52:12 +0100

    Create model_training.py

---
+++ model_training.py
@@ -0,0 +1,49 @@
+#Training Step
+
+def make_train_step(model, loss_fn, optimizer):
+
+  def perform_train_step(X, y):
+
+    #Set model train
+    model.train()
+
+    yhat = model(X)
+    
+    loss = loss_fn(yhat, y)
+
+    loss.backward()
+
+    optimizer.step()
+    optimizer.zero_grad()
+
+    return loss.item()
+
+
+  return perform_train_step
+
+
+make_train_step(model, loss_fn, optimizer)
+
+
+n_epochs = 1000
+
+losses = []
+
+for epochs in range(n_epochs):
+  ##inner loop
+  mini_batches_losses = []
+  for x_batch, y_batch in train_loader:
+    #move datasets to device
+    x_batch = x_batch.to(device)
+    y_batch = y_batch.to(device)
+
+
+    #perform one train step and return the loss
+    mini_batch_loss = train_step(x_batch, y_batch)
+    mini_batches_losses.append(mini_batch_loss)
+
+
+  # Compute the average loss
+  loss = np.mean(mini_batches_losses)
+
+  losses.append(loss)
