8bf671f7c3ae4ae5442627733855ede899fb92ba
Elint Eerie <96844092+elinteerie@users.noreply.github.com>
2022-09-21 12:50:05 +0100

    Update model_training.py

--- model_training.py
+++ model_training.py
@@ -97,5 +97,29 @@ for epoch in range(n_epochs):
   with torch.inference_mode:
     val_loss = mini_batch(device, val_loader, val_step)
     val_losses.append(val_loss)
+    
+    
+    
+n_epochs = 200
+losses = []
+val_losses = []
+
+for epoch in range(n_epochs):
+  loss = mini_batch(device, train_loader, train_step)
+  losses.append(loss)
+
+
+  with torch.inference_mode():
+    val_loss = mini_batch(device, val_loader, val_step)
+    val_losses.append(val_loss)
+
+
+  writer.add_scalars(main_tag='loss',
+  tag_scalar_dict={'training':loss,
+  'validation':val_loss}, global_step= epoch)
+
+
+
+writer.close()
 
 
