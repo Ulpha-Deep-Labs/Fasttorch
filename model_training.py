e333857f43fbc65f92f99ec1b5235ef4343f9fa8
Elint Eerie <96844092+elinteerie@users.noreply.github.com>
2022-09-21 10:04:44 +0100

    Update model_training.py

--- model_training.py
+++ model_training.py
@@ -80,3 +80,22 @@ LOSS = np.mean(losses)
 print(f'Loss:{LOSS:.4f}')
 
 
+#model training
+n_epochs = 200
+
+losses = []
+val_losses = []
+
+for epoch in range(n_epochs):
+  loss = mini_batch(device, train_loader, train_step)
+  losses.append(loss)
+
+
+
+
+
+  with torch.inference_mode:
+    val_loss = mini_batch(device, val_loader, val_step)
+    val_losses.append(val_loss)
+
+
